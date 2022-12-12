dc2995b615aaf32d53442d67dad685aeb1d25c9a
MathieuDacheux <mathieu.dacheux@icloud.com>
2022-12-12 15:32:24 +0100

    Ajout filtrage dans menu

--- controllers/menuCtrl.php
+++ controllers/menuCtrl.php
@@ -4,9 +4,7 @@
     $isOnMenu = true;
     
     $typesOfDishes = Dish::dishTypes();
-    $allDishes = Dish::getAll();
-
-
+    
     include(__DIR__ . '/../views/templates/header.php');
     include(__DIR__ . '/../views/menu.php');
     include(__DIR__ . '/../views/templates/footer.php');
